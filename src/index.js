70e0b5037ba9632905f3c01682c5f94ad5629e66
anissalopez <anissa965@gmail.com>
2023-05-10 13:02:03 -0400

    events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+  
+      buildToDo(e.target['new-task-description'].value)
+    
+     // buildToDo(//e.targetvalue goes Headers)
+      form.reset()
+
+  })
+})
+
+function buildToDo(toDo){
+  let p = document.createElement('p')
+  p.textContent = ` ${toDo} `
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = `  x  `
+  document.getElementById('list').appendChild(p)
+  p.appendChild(btn)
+}
+
+function handleDelete (e){
+  e.target.parentNode.remove()
+}
